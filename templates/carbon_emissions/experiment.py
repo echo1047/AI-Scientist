ac0b107658d1ad4ca2789291dd1de177cdbe8e79
echo1047 <371038049@qq.com>
2024-10-24 23:16:16 +0800

    10-24

--- templates/carbon_emissions/experiment.py
+++ templates/carbon_emissions/experiment.py
@@ -1,5 +1,3 @@
-import os
-import pandas as pd
 import argparse
 
 
@@ -7,6 +5,6 @@ parser = argparse.ArgumentParser(description="Run experiment")
 parser.add_argument("--out_dir", type=str, default="run_0", help="Output directory")
 args = parser.parse_args()
 
-if __name__ == 'main':
-    csv_file = 'data.csv'
-    df = pd.read_csv(csv_file)
+
+if __name__ == "__main__":
+    
--- test.sh
+++ test.sh
@@ -1,1 +1,11 @@
-python launch_scientist.py --model "gpt-4o-mini-2024-07-18" --experiment nanoGPT_lite --num-ideas 1 --skip-novelty-check
+python launch_scientist.py --model "gpt-4o-mini-2024-07-18" \
+    --experiment carbon_emissions \
+    --num-ideas 2 \
+    # --skip-novelty-check \
+    # --skip-idea-generation
+
+# python launch_scientist.py --model "gpt-4o-mini-2024-07-18" --experiment nanoGPT_lite --num-ideas 2 --skip-idea-generation --skip-novelty-check
+
+# cd templates/carbon_emissions && python experiment.py --out_dir run_0 && python plot.py
+
+# python launch_scientist.py --model "gpt-4o-mini-2024-07-18" --experiment carbon_emissions --num-ideas 2 --skip-idea-generation --skip-novelty-check
